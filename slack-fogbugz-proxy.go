7a20ebf892899391f73a0f0f74e3e6ccf5e99830
Kim Blomqvist <kim.blomqvist@lekane.com>
2014-04-08 21:49:39 +0300

    cleanup & fixup

--- slack-fogbugz-proxy.go
+++ slack-fogbugz-proxy.go
@@ -17,22 +17,27 @@ type Message struct {
 	Channel  string `json:"channel"`
 	Username string `json:"username"`
 	Icon_url string `json:"icon_url"`
-	//icon_emoji string
 }
 
 func post(text string) {
 	m := Message{text, "#support", "fogbugz", "http://www.fogcreek.com/images/fogbugz/pricing/kiwi.png"}
 	b, err := json.Marshal(m)
-	fmt.Printf("%s => %s (err=%i) \n", m, b, err)
+	if err != nil {
+		fmt.Printf("ERROR '%s' marshalling message: %s\n", err, m)
+		return
+	}
+	fmt.Printf("Posting: %s\n", b)
 	http.Post(webhookurl, "text/json", bytes.NewReader(b))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Received: %s!\n", r.URL)
+	fmt.Printf("-------------\n")
+	fmt.Printf("Received: %s\n", r.URL)
 	s, _ := url.QueryUnescape(r.URL.String()[1:])
 	s = strings.Replace(s, "http:/", "http://", 1)
-	fmt.Printf("Decoded as: %s!\n", s)
+	fmt.Printf("Decoded as: %s\n", s)
 	post(s)
+	fmt.Printf("-------------\n")
 }
 
 func main() {
@@ -40,13 +45,14 @@ func main() {
 		fmt.Printf("Usage: %s <slack-webhook-url>\n", os.Args[0])
 		fmt.Printf("e.g: %s https://company.slack.com/services/hooks/incoming-webhook?token=loremipsum\n", os.Args[0])
 		fmt.Printf("\nConfigure a fogbugz URL Trigger like to send on Case events to:\n")
-		fmt.Printf("http://your-proxy-host:10333//{CaseNumber}: {EventType} - {AssignedToName} - <http:/your-fogbugz-host/default.asp?{CaseNumber}|{Title}>")
+		fmt.Printf("http://your-proxy-host:10333//{CaseNumber}: {EventType} - {AssignedToName} - <http:/your-fogbugz-host/default.asp?{CaseNumber}|{Title}>\n")
 		return
 	}
 	webhookurl = os.Args[1]
-	fmt.Printf("Using: %s\n", webhookurl)
+	port := ":10333"
+	fmt.Printf("Listening to port: %s\n", port)
 	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":10333", nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 	}
